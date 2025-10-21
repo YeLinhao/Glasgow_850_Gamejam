19b82cfa646412fa3eddc1e44d4266a031d7299e
Max MacDonald <2567568m@student.gla.ac.uk>
2025-10-21 16:54:05 +0100

    Added isometric view and fixed movement to accomodate. Started with player side object interaction code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class PlayerController : MonoBehaviour
 {
-
-    [SerializeField] private float speed = 5f;
-    [SerializeField] private float dashSpeed = 10f;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float dashSpeed = 20f;
     [SerializeField] private float gravity = -9.8f;
+    [SerializeField] private float turnSpeed = 360;
 
     private CharacterController controller;
     private Vector2 moveInput;
     private Vector3 velocity;
+    private Vector3 move;
+
+    private bool holdingObject = true;
+    private bool canDash;
+    private bool isDashing;
+
+    public float dashForce;
+    public float dashDuration;
+    
+    
 
     void Awake()
     {
@@ -24,21 +36,61 @@ public class PlayerController : MonoBehaviour
 
     public void Interact(InputAction.CallbackContext context)
     {
-    // Interaction logic goes here
+        if (holdingObject)
+        {
+            if (context.performed)
+            {
+                speed = speed / 4;
+                // Collect start time
+            }
+            if (context.action.WasReleasedThisFrame())
+            {
+                speed = speed * 4;
+                // if object is throwable then throw here with power = duration held
+                
+            }
+        }
+        else
+        {
+            //Check for object in front arc, if object is grabbable then grab object
+        }
+        
     }
 
     public void Dash(InputAction.CallbackContext context)
     {
-        // Dash logic goes here
+        if (context.performed)
+        {
+            Debug.Log("X pressed");
+            canDash = false;
+            isDashing = true;
+            rb.MovePosition(transform.position + (transform.forward * move.normalized.magnitude * dashForce) * speed * Time.deltaTime);
+
+        }
     }
 
-    void Update()
+
+    private void Look()
     {
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
-        controller.Move(move * speed * Time.deltaTime);
+        
+        if (move == Vector3.zero) return;
+        var matrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+        var skewedInput = matrix.MultiplyPoint3x4(move);
 
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        var relative = (transform.position + skewedInput) - transform.position;
+        var rot = Quaternion.LookRotation(relative, Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, turnSpeed * Time.deltaTime);
+    }
 
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        move = new Vector3(moveInput.x, 0, moveInput.y);
+        Look();
+        rb.MovePosition(transform.position + (transform.forward * move.normalized.magnitude) * speed * Time.deltaTime);
     }
 }
