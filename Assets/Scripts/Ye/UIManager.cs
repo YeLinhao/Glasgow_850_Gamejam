5dbd233668fafed307116d41ed23a2da9d86aa91
Linhao Ye <130962468+YeLinhao@users.noreply.github.com>
2025-10-25 02:09:54 +0800

    ScoreSystemUI

--- Assets/Scripts/Ye/UIManager.cs
+++ Assets/Scripts/Ye/UIManager.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     //Set as Singleton
     public static UIManager Instance;
+
+
     void Awake()
     {
         if (Instance == null)
@@ -14,6 +18,12 @@ public class UIManager : MonoBehaviour
     }
 
     public GameObject LeaderboardPanel;
+    public TMP_Text FirstPlace_Score;
+    public TMP_Text SecondPlace_Score;
+    public TMP_Text ThirdPlace_Score;
+
+
+
 
     public void UI_EnableLeaderboard()
     {
