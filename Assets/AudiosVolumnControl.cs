3e4ce2fe1f14ea2a2bcc0becc853af5a6a790341
Linhao Ye <130962468+YeLinhao@users.noreply.github.com>
2025-11-05 01:21:31 +0800

    tried settings but not working

--- Assets/AudiosVolumnControl.cs
+++ Assets/AudiosVolumnControl.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class AudiosVolumnControl : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        this.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("MainVolume");
-    }
+    //void Start()
+    //{
+    //    this.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("MainVolume");
+    //}
 
 }
--- Assets/MusicVolumnControl.cs
+++ Assets/MusicVolumnControl.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 public class MusicVolumnControl : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        this.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("MusicVolume");
-    }
+    //void Start()
+    //{
+    //    this.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("MusicVolume");
+    //}
+
+    //void Update()
+    //{
+    //    this.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("MusicVolume");
+    //}
 
 }
